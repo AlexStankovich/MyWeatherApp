5400640eab929079985bbd1a61294ff1e85f38bf
Aleksandar Stankovic <a.stankovic994@gmail.com>
2021-12-01 03:46:47 +0100

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,22 @@
+const key = "IKhfu5nV2le5ciJnI07iBNySEMb1amGH";
+
+// get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+// get weather information
+
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
